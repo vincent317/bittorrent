628e2bc11eeaa154a2accf3d7fe57661b855225d
Thong Do <thobado1999@gmail.com>
2020-12-09 18:50:17 -0500

    file assembler

---
+++ file_assembler.c
@@ -0,0 +1,99 @@
+#include "file_assembler.h"
+
+void file_assembler_begin(Torrent * torrent){
+    TorrentFile * currentFile = torrent->files;
+
+    uint64_t total = 0;
+    uint64_t upperLimit = 0;
+    uint64_t lowerLimit = 0;
+    uint64_t pieceIndex = 0;
+    uint32_t pieceSize = torrent->piece_length;
+
+    while(currentFile != NULL){
+        FILE * fp;
+        FILE * piece;
+
+        upperLimit = upperLimit + currentFile->file_len;
+
+        TorrentFilePath * path = currentFile->path;
+        char backPath[2048];
+        chdir('./');
+        while(path->next != NULL){
+            mkdir(path->component, 0777);
+
+            strcat(backPath, '../');
+            chdir(path->component);
+            path = path->next;
+        }
+        chdir(backPath);
+        
+        fp = fopen(currentFile->full_path, "a");
+
+        while(total < upperLimit){
+            const char * filename = sha1_to_hexstr(torrent->piece_hashes[pieceIndex]);
+            char filePath[256];
+            memset(filePath, 0, sizeof(char) * 256);
+            filePath[0] = '\0';
+            strcat(filePath, "./.torrent_data/");
+            strcat(filePath, torrent->hash_str);
+            strcat(filePath, "/");
+            strcat(filePath, filename);
+            piece = fopen(filePath, "r");
+
+            fseek(piece, 0L, SEEK_END);
+            uint64_t fileSize = ftell(piece);
+            rewind(piece);
+
+            int v = total + fileSize;
+            
+            if(pieceIndex * pieceSize < lowerLimit){
+                // Add upper part of piece
+                int offset = (lowerLimit - (pieceIndex * pieceSize));
+                int amountGet = fileSize - offset;
+                read_then_write(piece, fp, offset, amountGet);
+
+                pieceIndex++;
+                total += amountGet;
+            }
+            else if(v > upperLimit){
+                // Add lower part of piece
+                int amountGet = upperLimit - (pieceIndex * pieceSize);
+                read_then_write(piece, fp, 0, amountGet);
+
+                total += amountGet;
+            }
+            else if(lowerLimit < v && v <= upperLimit){
+                // Add whole piece
+                read_then_write(piece, fp, 0, fileSize);
+
+                pieceIndex++;
+                total += fileSize;
+            }
+
+            fclose(piece);
+            free(filename);
+        }
+
+        lowerLimit = upperLimit;
+        currentFile = currentFile->next_file;
+        fclose(fp);
+    }
+
+}
+
+
+void file_assembler_read_then_write(FILE * readFile, FILE * writeFile, uint64_t beginIndex, uint64_t amount){
+    uint8_t buffer[2048];
+    uint64_t amountLeft = amount;
+    if(beginIndex > 0){
+        // Begin reading from offset beginIndex
+        fseek(readFile, beginIndex, SEEK_SET);
+    }
+
+    while(amountLeft > 0){
+        int readAmount = 2064 < amountLeft ? 2064 : amountLeft;
+        fread(buffer, 1, readAmount, readFile);
+        fwrite(buffer, 1, readAmount, writeFile);
+        amountLeft = amountLeft - readAmount;
+    }
+}
--- file_assembler.h
+++ file_assembler.h
@@ -2,6 +2,18 @@
 #ifndef FILE_ASSEMBLER_H_INCLUDED
 #define FILE_ASSEMBLER_H_INCLUDED
 
-// TODO
+#include <sys/types.h> 
+#include <sys/stat.h> 
+
+#include "shared.h"
+#include "torrent_runtime.h"
+#include "peer_manager.h"
+#include "upload_download_manager.h"
+
+// Assemble the piece into files
+void file_assembler_begin(Torrent * torrent);
+
+// Read amount of byte from readFile and write it to writeFile starting at index beginIndex
+void file_assembler_read_then_write(FILE * readFile, FILE * writeFile, uint64_t beginIndex, uint64_t amount);
 
 #endif
